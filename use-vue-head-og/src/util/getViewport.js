9f77e7a113bc7954843352b19bc6e719131996ba
심소정 <opmiu@mz.co.kr>
2019-07-12 13:12:22 +0900

    semi 제거

--- use-vue-head-og/src/util/getViewport.js
+++ use-vue-head-og/src/util/getViewport.js
@@ -1,8 +1,8 @@
-import isMobile from './checkUserAgent';
-import { cloneDeep } from 'lodash';
+import isMobile from './checkUserAgent'
+import { cloneDeep } from 'lodash'
 
 export function getViewport(defaultMetaInfo) {
-  const newMetaInfo = cloneDeep(defaultMetaInfo);
+  const newMetaInfo = cloneDeep(defaultMetaInfo)
   if (newMetaInfo.meta) {
     newMetaInfo.meta.push({
       name: 'viewport',
@@ -13,7 +13,7 @@ export function getViewport(defaultMetaInfo) {
               'width=720, user-scalable=yes, target-densitydpi=device-dpi;'
           }
         : { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }
-    });
+    })
   }
-  return newMetaInfo;
+  return newMetaInfo
 }
